ee80a992d33f06ed170ee964da8f3758cf5659f6
Benjamin Bertrand <benjamin.bertrand@maxiv.lu.se>
2026-05-13 18:32:07 +0200

    Add debug output

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,17 @@
-mkdir build
-cd build
 cmake ${CMAKE_ARGS} \
-      -DCMAKE_BUILD_TYPE=Release \
-      ..
+      -G Ninja \
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+      -S . \
+      -B build
 
-make -j $CPU_COUNT
-make install
+cmake --build build
+cmake --build build --target install
+
+# Separate debugging symbols on Linux
+if [ -n "${OBJCOPY}" ]
+then
+  ${OBJCOPY} --only-keep-debug ${PREFIX}/lib/libhdb++.so.${LIBHDBPP_VERSION} ${PREFIX}/lib/libhdb++.so.${LIBHDBPP_VERSION}.dbg
+  chmod 664 ${PREFIX}/lib/libhdb++.so.${LIBHDBPP_VERSION}.dbg
+  ${OBJCOPY} --strip-debug ${PREFIX}/lib/libhdb++.so.${LIBHDBPP_VERSION}
+  ${OBJCOPY} --add-gnu-debuglink=${PREFIX}/lib/libhdb++.so.${LIBHDBPP_VERSION}.dbg ${PREFIX}/lib/libhdb++.so.${LIBHDBPP_VERSION}
+fi
